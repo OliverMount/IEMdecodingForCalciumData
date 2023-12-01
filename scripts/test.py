2d59318f566096ff300067b33db20e6557eefb65
OliverMount <oliverjmount@yahoo.com>
2023-12-01 17:11:11 +0900

    updating

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,25 @@
+tr=40
+
+ho=homo_data_p[:,:,tr]
+he=hetero_data_p[:,:,tr]
+ho_la=homo_labels 
+he_la=hetero_labels 
+Info=PrefDirInfo
+
 def run_parallel_the_pop_decoding(ho,he,ho_la,he_la,Info,nt):
 	
 	n_cpu = mp.cpu_count()  # Total number of CPU
 	pool = mp.Pool(n_cpu) 
 	
-	time_step_results = [pool.apply_async(pop_decode_at_a_single_timept,args=(ho,he,ho_la,he_la,Info)) for tr in range(nt)]   
+	time_step_results = [pool.apply_async(pop_decode_at_a_single_timept,args=(ho[:,:,tr],he[:,:,tr],ho_la,he_la,Info)) for tr in range(nt)]   
 	pool.close()
 	pool.join()
 
 	results = [r.get() for r in time_step_results]   
  
-	return np.stack(results,axis=-1).transpose(0,2,1)   # no X time X (homo,hetero)
+	return np.stack(results,axis=-1)  # neurons X presented stimuls X (mean, std) X time    
 
-
+ 
 
 def pop_decode_at_a_single_timept(ho,he,ho_la,he_la,Info):
 	
@@ -22,25 +30,50 @@ def pop_decode_at_a_single_timept(ho,he,ho_la,he_la,Info):
 	(population curve construction)
 	"""
 	
-    # In a given trial a stimuls is presented 
+	# initialization for mean and standard devivation
+	res_ho_mean=np.zeros((ns,ns))
+	res_he_mean=np.zeros_like(res_ho) 
+	res_ho_std=np.zeros_like(res_ho)
+	res_he_std=np.zeros_like(res_ho)
 	
+	# In a given trial a stimuls is presented  
 	for k in range(1,ns+1):  # Given a stimulus (directions are coded from 1 to 8)
-        
-        # presented trials
-        homo_trials=np.where(ho_la==k)[0]  # cannot be empty
-        hetero_trials=np.where(he_la==k)[0] # cannot be empty
-    
-        # subset the data based on trials
-        ho_subset_1=ho[:,homo_trials]
-        h2_subset_1=he[:,hetero_trials]
-        
-        # For each tuning neuron (subset the data based on neurons)
-        for l in range(1,ns): 
-            idx_homo=np.where(Info['Pref.Homo']==l)[0]
-            idx_hetero=np.where(Info['Pref.Hetero']==l)[0] 
+		
+		# presented trials
+		homo_trials=np.where(ho_la==k)[0]  # cannot be empty
+		hetero_trials=np.where(he_la==k)[0] # cannot be empty
+	
+		# subset the data based on trials
+		ho_subset_1=ho[:,homo_trials]
+		he_subset_1=he[:,hetero_trials]
+		
+		# For each tuning neuron (subset the data based on neurons)
+		for l in range(1,ns): 
+			idx_homo=np.where(Info['Pref.Homo']==l)[0]
+			idx_hetero=np.where(Info['Pref.Hetero']==l)[0] 
+			
+			# subsetted data based on the tuned neurons (if they exists)
+			if len(idx_homo)!=0:
+				ho_subset2=ho_subset_1[idx_homo,:]
+				ho_mean=np.mean(ho_subset2.flatten())
+				ho_std=np.std(ho_subset2.flatten())
+			else: # if neuron group does not exist
+				ho_mean=0
+				ho_std=0
+			if len(idx_hetero)!=0:
+				he_subset2=he_subset_1[idx_hetero,:]
+				he_mean=np.mean(he_subset2.flatten())
+				he_std=np.std(he_subset2.flatten())
+			else:
+				he_mean=0
+				he_std=0
+				 
+			res_ho_mean[l-1,k-1]=ho_mean
+			res_he_mean[l-1,k-1]=he_mean
+
+			res_ho_std[l-1,k-1]=ho_std
+			res_he_std[l-1,k-1]=he_std
+			
             
-            # subsetted data based on the tuned neurons
-            ho_subset=homo_data[idx_homo,:]
-            he_subset=homo_data[idx_homo,:]
-    
-		pass 
+    return np.stack((res_ho_mean,res_he_mean,res_ho_std,res_he_std),axis=-1) 
+	
