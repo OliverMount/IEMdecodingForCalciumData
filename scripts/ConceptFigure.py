bcb6703e589242cbc01d794796bb3ae321360e08
OliverMount <oliverjmount@yahoo.com>
2023-12-07 13:45:02 +0900

    updating

--- scripts/ConceptFigure.py
+++ scripts/ConceptFigure.py
@@ -3,63 +3,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set a seed for reproducibility
+ 
 #np.random.seed(42)
 
-# Mean values
+fig_size=(7, 5)
+
+ 
 mean_values = np.arange(22.5, 360, 45)
 num_points=len(mean_values)
-# Number of neruons per stimulus
-num_curves = 2 
 
+num_curves = 2 # Number of neruons per stimulus 
 # Initialize an array to store Gaussian curves
 gaussian_curves = np.zeros((num_points, num_points * num_curves))
+ 
+
+fig, ax = plt.subplots(1, figsize=fig_size)
 
 
-
-fig, ax = plt.subplots(1, figsize=(17, 5))
-
-
-# Generate and plot Gaussian curves
-#colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange']
+ 
+ 
 colors=  ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#17becf']
 
 for idx, mean in enumerate(mean_values):
-    for curve_num in range(num_curves):
-        # Randomly choose standard deviation in the range (10, 20)
-        std_dev = np.random.uniform(40, 50)
-
-        # Randomly choose peak amplitude in the range (1, 10)
-        peak_amplitude = np.random.uniform(1, 10)
-
-        # Use mean_values directly instead of generating x values
-        x = mean_values
-
-        # Generate Gaussian curve
-        y = peak_amplitude * np.exp(-(x - mean)**2 / (2 * std_dev**2))
-        
-        # Store the Gaussian curve in the array
-        gaussian_curves[:, idx * num_curves + curve_num] = y
-
-        # Plot only the data points at the mean with a smaller and unfilled marker
-        ax.scatter(x, y, color=colors[idx], marker='o', s=25)
-
-        # Plot the entire curve
+    for curve_num in range(num_curves): 
+        std_dev = np.random.uniform(40, 50) 
+        peak_amplitude = np.random.uniform(1, 10) 
+        x = mean_values 
+        y = peak_amplitude * np.exp(-(x - mean)**2 / (2 * std_dev**2)) 
+        gaussian_curves[:, idx * num_curves + curve_num] = y 
+        ax.scatter(x, y, color=colors[idx], marker='o', s=25) 
         ax.plot(x, y, color=colors[idx], alpha=0.8,lw=3)
-
-# Set x-axis ticks to show mean values
-ax.set_xticks([])
-
-# Add vertical lines to mark mean values
+ 
+ 
+ax.set_xticks([]) 
 for mean in mean_values:
     ax.axvline(x=mean, color='gray', linestyle='--', alpha=0.5) 
-
-# Set y-axis limits to ensure zero points are clearly visible
-ax.set_ylim(-0.1,10)
-
-# Set labels and title
-#ax.set_xlabel('Mean motion direction')
-#ax.set_ylabel('Tuning curve amplitude (a.u)')
+ 
+ax.set_ylim(-0.1,10) 
+ 
 ax.spines[['top', 'right']].set_visible(False)
 ax.spines[['bottom', 'left']].set_linewidth(3)
 ax.tick_params(axis='both', which='major', left=False, right=False, labelleft=False)
@@ -78,11 +59,11 @@ flattened_values = reshaped_values.flatten()
 x_positions = np.arange(0, 8)
 
 
-fig, ax = plt.subplots(1, figsize=(17, 5))
+fig, ax = plt.subplots(1, figsize=fig_size)
 # Plot the scatter plot
 for k in range(len(x_positions)):
 	ax.scatter(np.repeat(x_positions[k], 2), flattened_values[2*k:2*k+2], color=colors[k], marker='o')
-ax.plot(averaged_row, color='k', linestyle='-', linewidth=2,marker='o',alpha=0.3)
+ax.plot(averaged_row, color='k', linestyle='-', linewidth=3,marker='o',alpha=0.3)
 
 ax.set_xticks([])
 ax.spines[['top', 'right']].set_visible(False)
@@ -93,8 +74,3 @@ ax.set_ylim(-0.1,10)
 fig.tight_layout(pad=2)  
 plt.show()
 fig.savefig("/home/olive/Desktop/Homo_PopTuning.tiff",dpi=300)
-
- 
-
-
-
